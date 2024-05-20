001f09e39cb9f4d009b1746cad2cc65332566842
amangreen123 <47338010+amangreen123@users.noreply.github.com>
2024-05-20 15:54:03 -0400

    Reading from file

---
+++ wcTool.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+int main() {
+
+
+	std::string line;
+	std::ofstream myfile;
+
+	myfile.open("example.txt");
+
+	if (myfile.is_open()) {
+	}
+}
