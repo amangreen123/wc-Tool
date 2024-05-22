cc41828623363d0d5fbd454b748b3ce026e28f0c
amangreen123 <47338010+amangreen123@users.noreply.github.com>
2024-05-22 10:57:39 -0400

    Trying to open file

--- wcTool.cpp
+++ wcTool.cpp
@@ -6,12 +6,14 @@
 
 int main() {
 
-
 	std::string line;
 	std::ofstream myfile;
 
 	myfile.open("example.txt");
 
 	if (myfile.is_open()) {
+
+		std::cout << "Enter text. To end, type 'end'." << std::endl;
+
 	}
 }
